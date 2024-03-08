99d332b4b8bde0647d895ac27cda4e64b7cf46e9
Nenogzar <stoyan.naidenov@gmail.com>
2024-03-08 08:00:18 +0200

    PG

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Password-generator
 make a strong password that will be automatically copied to the clipboard
+
+
+> If you need a strong password, just run and you'll have a ready password copied to the clipboard
+
+```Python 
+Usage:
+  generate-password [-u] [-m] [-n] [-s] [-l <length>] [-c] [options]
+
+Options:
+  -u, --no-uppercase             Exclude uppercase letters. (default: false)
+  -m, --no-minuscule             Exclude miniscule/lowercase letters. (default: false)
+  --no-lowercase                 Another name for -m or --no-minuscule. (default: false)
+  --no-miniscule                 Common typo for --no-minuscule. (default: false)
+  -n, --no-numbers               Exclude numbers. (default: false)
+  -s, --no-symbols               Exclude special symbols. (default: false)
+  -l <length>, --len <length>    Password length (default: 12).
+  -c, --dont-copy                Don't copy the password to the clipboard. (default: false)
+  -h, --help                     Show this help message and exit.
+
+Examples:
+  $ generate-password
+  TEm{m3{;Csvm
+
+  $ generate-password -s -l 4
+  Va9U
+
+  $ generate-password -s -l 8 -c
+  BxSuSL14  # copied to the clipboard
+```
---
+++ pass_gen.py
@@ -0,0 +1,58 @@
+import random
+import string
+import pyperclip
+from docopt import docopt
+try:
+    from icecream import ic
+except ImportError:  # Graceful fallback if IceCream isn't installed.
+    ic = lambda *a: None if not a else (a[0] if len(a) == 1 else a)  # noqa
+
+
+def generatePassword(
+        uppercase=True, lowercase=True, numbers=True, symbols=False, length=8):
+    password = ''
+    alphabet = ''
+
+    if uppercase:
+        alphabet += string.ascii_uppercase
+        password += random.choice(string.ascii_uppercase)
+    if lowercase:
+        alphabet += string.ascii_lowercase
+        password += random.choice(string.ascii_lowercase)
+    if numbers:
+        alphabet += string.digits
+        password += random.choice(string.digits)
+    if symbols:
+        alphabet += string.punctuation
+        password += random.choice(string.punctuation)
+
+    if not alphabet:
+        raise ValueError('You must enable at least one character set.')
+
+    remainingLength = length - len(password)
+    if remainingLength <= 0:
+        return password
+
+    password += ''.join(random.choice(alphabet) for _ in range(remainingLength))
+    password = ''.join(random.sample(password, len(password)))  # shuffle
+
+    return password
+
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+
+    uppercase = not args['--no-uppercase']
+    lowercase = not any(
+        args[k] for k in ['--no-lowercase', '--no-minuscule', '--no-miniscule'])
+    numbers = not args['--no-numbers']
+    symbols = not args['--no-symbols']
+    length = int(args.get('--len') or 8)
+    copyToClipboard = not args['--dont-copy']
+
+    password = generatePassword(uppercase, lowercase, numbers, symbols, length)
+    ic(password)
+
+    if copyToClipboard:
+        pyperclip.copy(password)
+
